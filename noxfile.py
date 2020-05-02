9d472e8473a4e0f2c806e2440ec401b0f55c5a85
sander <s.teunissen@gmail.com>
2020-05-02 07:53:17 +0200

    updating nox file

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,7 @@ def install_flit_dev_deps(session):
     session.run("flit", "install", "--deps", "develop")
 
 
-@nox.session(python=["3.6", "3.7", "3.8"])
+@nox.session(python=["3.7", "3.8"])
 def tests(session):
     install_flit_dev_deps(session)
-    session.run("pytest", "--cov=mspyteams", "--cov-report=xml:cov.xml", "tests")
+    session.run("pytest", "--cov=aiosubpub", "--cov-report=xml:cov.xml", "tests")
